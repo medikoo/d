ffe248901f99bb83c077ecec56dcfafea004eb7c
Mariusz Nowak <mariusz@medikoo.com>
2013-10-23 16:30:50 +0200

    Improve d.lazy
    - Option to make it cross prototype safe with side property cache
      (name provided through `cacheName` option)
    - Option `desc`, to provide custom descriptor for calculated/exposed property

--- lazy.js
+++ lazy.js
@@ -4,24 +4,50 @@ var copy       = require('es5-ext/object/copy')
   , map        = require('es5-ext/object/map')
   , callable   = require('es5-ext/object/valid-callable')
   , validValue = require('es5-ext/object/valid-value')
+  , contains   = require('es5-ext/string/#/contains')
 
   , defineProperty = Object.defineProperty
   , hasOwnProperty = Object.prototype.hasOwnProperty
+  , cacheDesc = { configurable: false, enumerable: false, writable: false,
+		value: null }
   , define;
 
 define = function (name, desc) {
-	var value, dgs;
+	var value, dgs, cacheName = desc.cacheName, expose;
 	validValue(desc);
 	value = callable(desc.value);
 	dgs = copy(desc);
 	delete dgs.writable;
 	delete dgs.value;
+	if (cacheName == null) cacheName = name;
 	dgs.get = function () {
+		if (name !== cacheName) {
+			if (hasOwnProperty.call(this, cacheName)) return this[cacheName];
+			cacheDesc.value = value.call(this);
+			defineProperty(this, cacheName, cacheDesc);
+			cacheDesc.value = null;
+			if (expose) defineProperty(this, name, desc);
+			return this[cacheName];
+		}
 		if (hasOwnProperty.call(this, name)) return value;
 		desc.value = value.call(this);
 		defineProperty(this, name, desc);
+		desc.value = null;
 		return this[name];
 	};
+	if (!desc.desc) return dgs;
+	desc = {
+		configurable: contains.call(desc.desc, 'c'),
+		enumerable: contains.call(desc.desc, 'e'),
+		writable: contains.call(desc.desc, 'w')
+	};
+	if (cacheName === name) {
+		desc.value = null;
+	} else {
+		delete desc.writable;
+		desc.get = dgs.get;
+	}
+	expose = true;
 	return dgs;
 };
 
--- test/lazy.js
+++ test/lazy.js
@@ -1,14 +1,36 @@
 'use strict';
 
-var d = require('../d');
+var d = require('../d')
+
+  , getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
 
 module.exports = function (t, a) {
-	var Foo = function () {}, i = 1, o;
+	var Foo = function () {}, i = 1, o, o2, desc;
 	Object.defineProperties(Foo.prototype, t({
 		bar: d(function () { return ++i; }),
-		bar2: d(function () { return this.bar + 23; })
+		bar2: d(function () { return this.bar + 23; }),
+		bar3: d(function () { return this.bar2 + 34; }, { desc: 'ew' }),
+		bar4: d(function () { return this.bar3 + 12; }, { cacheName: '_bar4_' }),
+		bar5: d(function () { return this.bar4 + 3; },
+			{ cacheName: '_bar5_', desc: 'e' })
 	}));
 
 	o = new Foo();
-	a.deep([o.bar, o.bar2], [2, 25]);
+	a.deep([o.bar, o.bar2, o.bar3, o.bar4, o.bar5], [2, 25, 59, 71, 74],
+		"Values");
+
+	a.deep(getOwnPropertyDescriptor(o, 'bar3'), { configurable: false,
+		enumerable: true, writable: true, value: 59 }, "Desc");
+	a(o.hasOwnProperty('bar4'), false, "Cache not exposed");
+	desc = getOwnPropertyDescriptor(o, 'bar5');
+	a.deep(desc, { configurable: false,
+		enumerable: true, get: desc.get, set: desc.set }, "Cache & Desc: desc");
+
+	o2 = Object.create(o);
+	o2.bar = 30;
+	o2.bar3 = 100;
+
+	a.deep([o2.bar, o2.bar2, o2.bar3, o2.bar4, o2.bar5], [30, 25, 100, 112, 115],
+		"Extension Values");
+
 };
