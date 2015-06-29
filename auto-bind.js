1886cb5c8e11b454c4577557d5c719a77ce0437d
Mariusz Nowak <medyk@medikoo.com>
2015-06-29 20:58:10 +0200

    Pass options directly

--- auto-bind.js
+++ auto-bind.js
@@ -1,23 +1,24 @@
 'use strict';
 
-var copy           = require('es5-ext/object/copy')
-  , ensureCallable = require('es5-ext/object/valid-callable')
-  , map            = require('es5-ext/object/map')
-  , callable       = require('es5-ext/object/valid-callable')
-  , validValue     = require('es5-ext/object/valid-value')
+var copy             = require('es5-ext/object/copy')
+  , normalizeOptions = require('es5-ext/object/normalize-options')
+  , ensureCallable   = require('es5-ext/object/valid-callable')
+  , map              = require('es5-ext/object/map')
+  , callable         = require('es5-ext/object/valid-callable')
+  , validValue       = require('es5-ext/object/valid-value')
 
   , bind = Function.prototype.bind, defineProperty = Object.defineProperty
   , hasOwnProperty = Object.prototype.hasOwnProperty
   , define;
 
-define = function (name, desc, resolveContext) {
+define = function (name, desc, options) {
 	var value = validValue(desc) && callable(desc.value), dgs;
 	dgs = copy(desc);
 	delete dgs.writable;
 	delete dgs.value;
 	dgs.get = function () {
 		if (hasOwnProperty.call(this, name)) return value;
-		desc.value = bind.call(value, resolveContext ? resolveContext(this) : this);
+		desc.value = bind.call(value, options.resolveContext ? options.resolveContext(this) : this);
 		defineProperty(this, name, desc);
 		return this[name];
 	};
@@ -25,8 +26,7 @@ define = function (name, desc, resolveContext) {
 };
 
 module.exports = function (props/*, options*/) {
-	var options = Object(arguments[1])
-	  , resolveContext;
-	if (options.resolveContext != null) resolveContext = ensureCallable(options.resolveContext);
-	return map(props, function (desc, name) { return define(name, desc, resolveContext); });
+	var options = normalizeOptions(arguments[1])
+	if (options.resolveContext != null) ensureCallable(options.resolveContext);
+	return map(props, function (desc, name) { return define(name, desc, options); });
 };
