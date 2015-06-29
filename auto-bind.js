d6090f49d653d76a65696e3c5fe8417c673c398a
Mariusz Nowak <medyk@medikoo.com>
2015-06-29 20:06:35 +0200

    Reconfigure bindTo support into options.resolveContext

--- auto-bind.js
+++ auto-bind.js
@@ -1,31 +1,32 @@
 'use strict';
 
-var copy       = require('es5-ext/object/copy')
-  , map        = require('es5-ext/object/map')
-  , callable   = require('es5-ext/object/valid-callable')
-  , validValue = require('es5-ext/object/valid-value')
+var copy           = require('es5-ext/object/copy')
+  , ensureCallable = require('es5-ext/object/valid-callable')
+  , map            = require('es5-ext/object/map')
+  , callable       = require('es5-ext/object/valid-callable')
+  , validValue     = require('es5-ext/object/valid-value')
 
   , bind = Function.prototype.bind, defineProperty = Object.defineProperty
   , hasOwnProperty = Object.prototype.hasOwnProperty
   , define;
 
-define = function (name, desc, bindTo) {
+define = function (name, desc, resolveContext) {
 	var value = validValue(desc) && callable(desc.value), dgs;
 	dgs = copy(desc);
 	delete dgs.writable;
 	delete dgs.value;
 	dgs.get = function () {
 		if (hasOwnProperty.call(this, name)) return value;
-		desc.value = bind.call(value, (bindTo == null) ? this : this[bindTo]);
+		desc.value = bind.call(value, resolveContext ? resolveContext(this) : this);
 		defineProperty(this, name, desc);
 		return this[name];
 	};
 	return dgs;
 };
 
-module.exports = function (props/*, bindTo*/) {
-	var bindTo = arguments[1];
-	return map(props, function (desc, name) {
-		return define(name, desc, bindTo);
-	});
+module.exports = function (props/*, options*/) {
+	var options = Object(arguments[1])
+	  , resolveContext;
+	if (options.resolveContext != null) resolveContext = ensureCallable(options.resolveContext);
+	return map(props, function (desc, name) { return define(name, desc, resolveContext); });
 };
